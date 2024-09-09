a05e465e8fe5d1a3ecc8df50afe8b7da3b32a3ac
Luyanda Mazibuko <ST10176153@rcconnect.edu.za>
2024-09-09 14:38:36 +0000

    Add project files.

---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace CMCSPT.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        public IActionResult ImageForm() {
+            return View();
+                }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMCSPT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
