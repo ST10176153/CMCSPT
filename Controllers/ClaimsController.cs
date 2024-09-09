e9b1944368c13962ff59a70b46f77b8605ccaac5
Luyanda Mazibuko <ST10176153@rcconnect.edu.za>
2024-09-09 14:39:56 +0000

    added Comments

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -3,6 +3,7 @@ namespace CMCSPT.Controllers
 {
     public class ClaimsController : Controller
     {
+        //Action to display the image form
         public IActionResult ImageForm() {
             return View();
                 }
